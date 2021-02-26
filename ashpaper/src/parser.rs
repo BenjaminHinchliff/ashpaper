81c739f66f1fd1538f12b373a6b6e7555f892d49
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-25 20:55:10 -0800

    improve syllable counting

--- ashpaper/src/parser.rs
+++ ashpaper/src/parser.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use cmudict_fast::{self as cmudict, Cmudict};
 use lazy_static::lazy_static;
 use regex::Regex;
@@ -44,123 +46,141 @@ lazy_static! {
     static ref VOWEL_CLUSTER_RE: Regex = Regex::new(r"[^aeiouy]+").unwrap();
     // * no error handling
     static ref CMUDICT: Cmudict = Cmudict::new("cmudict.dict").unwrap();
-} 
+}
 
-    /// test for alliteration by checking if multiple words in the input
-    /// start with the same letter
-    fn has_alliteration(input: &str) -> bool {
-        let lower_input = input.to_lowercase();
-        let mut input_iter = lower_input.split(' ').filter(|w| !w.is_empty());
+/// test for alliteration by checking if multiple words in the input
+/// start with the same letter
+fn has_alliteration(input: &str) -> bool {
+    let lower_input = input.to_lowercase();
+    let mut input_iter = lower_input.split(' ').filter(|w| !w.is_empty());
 
-        if let Some(start_word) = input_iter.next() {
-            let mut cur_start_letter = start_word.chars().next().unwrap();
-            for word in input_iter {
-                if word.starts_with(cur_start_letter) {
-                    return true;
-                }
-                cur_start_letter = word.chars().next().unwrap();
+    if let Some(start_word) = input_iter.next() {
+        let mut cur_start_letter = start_word.chars().next().unwrap();
+        for word in input_iter {
+            if word.starts_with(cur_start_letter) {
+                return true;
             }
+            cur_start_letter = word.chars().next().unwrap();
         }
-        false
     }
+    false
+}
 
-    fn approximate_syllables(word: &str) -> usize {
-        let clusters: Vec<_> = VOWEL_CLUSTER_RE.split(word).collect();
-        println!("{:?}", clusters);
-        0
-    }
-
-    fn count_word_syllables(word: &str) -> usize {
-        // TODO: add handling if not in cmudict
-        if let Some(rule) = CMUDICT.get(word) {
-            let pronunciation = rule.pronunciation();
-            // TODO: inconsitent with python interpeter due to varying lengths of cmudict pronunciations
-            pronunciation.iter().filter(|po| po.is_syllable()).count()
+fn approximate_syllables(word: &str) -> usize {
+    let clusters: Vec<_> = VOWEL_CLUSTER_RE.split(word).collect();
+    const DIPHTHONGS: &[&'static str] = &[
+        "ai", "au", "ay", "ea", "ee", "ei", "ey", "oa", "oe", "oi", "oo", "ou", "oy", "ua", "ue",
+        "ui",
+    ];
+    println!("{:?}", clusters);
+    let mut count: usize = 0;
+    for cluster in clusters {
+        count += if DIPHTHONGS.contains(&cluster) {
+            1
         } else {
-            approximate_syllables(word)
+            cmp::min(2, cluster.len())
         }
     }
+    count
+}
 
-    fn count_syllables(input: &str) -> usize {
-        input
-            .split(' ')
-            .filter(|w| !w.is_empty())
-            .map(|w| count_word_syllables(w))
-            .sum()
+fn count_word_syllables(word: &str) -> usize {
+    if let Some(rules) = CMUDICT.get(word) {
+        rules
+            .iter()
+            .map(|r| {
+                r.pronunciation()
+                    .iter()
+                    .filter(|po| po.is_syllable())
+                    .count()
+            })
+            .max()
+            .unwrap()
+    } else {
+        approximate_syllables(word)
     }
+}
 
-    fn check_end_rhyme(last_line_option: Option<&str>, cur_line: &str) -> bool {
-        if let Some(last_line) = last_line_option {
-            // end-rhyme handling
-            if let (Some(last_line_word), Some(last_word)) = (
-                last_line.split(' ').rev().next(),
-                cur_line.split(' ').rev().next(),
-            ) {
-                if let (Some(last_line_rule), Some(last_rule)) = (
-                    CMUDICT.get(last_line_word),
-                    CMUDICT.get(last_word),
-                ) {
-                    return cmudict::rhymes(&last_line_rule, &last_rule);
+fn count_syllables(input: &str) -> usize {
+    input
+        .split(' ')
+        .filter(|w| !w.is_empty())
+        .map(|w| count_word_syllables(w))
+        .sum()
+}
+
+fn check_end_rhyme(last_line_option: Option<&str>, cur_line: &str) -> bool {
+    if let Some(last_line) = last_line_option {
+        // end-rhyme handling
+        if let (Some(last_line_word), Some(last_word)) = (
+            last_line.split(' ').rev().next(),
+            cur_line.split(' ').rev().next(),
+        ) {
+            if let (Some(last_line_rule), Some(last_rule)) =
+                (CMUDICT.get(last_line_word), CMUDICT.get(last_word))
+            {
+                return cmudict::rhymes(&last_line_rule, &last_rule);
+            }
+        }
+    }
+    false
+}
+
+pub fn parse(input: &str) -> Vec<Instruction> {
+    let mut last_line_option: Option<&str> = None;
+    let mut lines = Vec::new();
+    for line in input.split('\n') {
+        // short-circuit on noop
+        if line != "" {
+            // everything else
+            let insType = if line.contains('/') {
+                InsType::Compare
+            } else if INT_CAP_RE.is_match(line) {
+                InsType::Negate
+            } else if CAP_RE.is_match(line) {
+                InsType::Multiply
+            } else if SIMILIE_RE.is_match(line) {
+                InsType::Add
+            } else if line.contains('?') {
+                InsType::PrintChar
+            } else if line.contains('.') {
+                InsType::Print
+            } else if line.contains(',') {
+                InsType::Pop
+            } else if line.contains('-') {
+                InsType::Push
+            } else if has_alliteration(line) {
+                InsType::Goto
+            } else if check_end_rhyme(last_line_option, line) {
+                println!("{:?}", last_line_option);
+                InsType::LineConditional {
+                    prev_syllables: count_syllables(last_line_option.unwrap()),
+                    cur_syllables: count_syllables(line),
                 }
-            }
+            } else {
+                InsType::Store(count_syllables(line))
+            };
+            let register = if WS_START_RE.is_match(line) {
+                Register::Register1
+            } else {
+                Register::Register0
+            };
+            let ins = Instruction {
+                instruction: insType,
+                register,
+                line: line.to_string(),
+            };
+            lines.push(ins);
         }
-        false
+        last_line_option = Some(line);
     }
-
-    pub fn parse(input: &str) -> Vec<Instruction> {
-        let mut last_line_option: Option<&str> = None;
-        let mut lines = Vec::new();
-        for line in input.split('\n') {
-            // short-circuit on noop
-            if line != "" {
-                // everything else
-                let insType = if line.contains('/') {
-                    InsType::Compare
-                } else if INT_CAP_RE.is_match(line) {
-                    InsType::Negate
-                } else if CAP_RE.is_match(line) {
-                    InsType::Multiply
-                } else if SIMILIE_RE.is_match(line) {
-                    InsType::Add
-                } else if line.contains('?') {
-                    InsType::PrintChar
-                } else if line.contains('.') {
-                    InsType::Print
-                } else if line.contains(',') {
-                    InsType::Pop
-                } else if line.contains('-') {
-                    InsType::Push
-                } else if has_alliteration(line) {
-                    InsType::Goto
-                } else if check_end_rhyme(last_line_option, line) {
-                    InsType::LineConditional {
-                        prev_syllables: count_syllables(last_line_option.unwrap()),
-                        cur_syllables: count_syllables(line),
-                    }
-                } else {
-                    InsType::Store(count_syllables(line))
-                };
-                let register = if WS_START_RE.is_match(line) {
-                    Register::Register1
-                } else {
-                    Register::Register0
-                };
-                let ins = Instruction {
-                    instruction: insType,
-                    register,
-                    line: line.to_string(),
-                };
-                lines.push(ins);
-            }
-            last_line_option = Some(line);
-        }
-        lines
-    }
+    lines
+}
 
 #[cfg(test)]
 mod tests {
+    use super::{InsType, Instruction, Register};
     use pretty_assertions::assert_eq;
-    use super::{Instruction, InsType, Register};
 
     #[test]
     fn has_alliteration() {
